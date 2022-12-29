fb2009bf982ca1c77b5f0ba892afe511ec0ce8cc
AnnKomissarova <nnkmssrv@gmail.com>
2022-12-29 15:53:01 +0300

    week-2

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -3,6 +3,14 @@ const addPhotoBtn = document.querySelector("#add-photo");
 const addPostBtn = document.querySelector("#add-first-post");
 const bodyOverlay = document.querySelector(".body-overlay");
 const body = document.querySelector("body");
+const publishBtn = document.querySelector("#post-publish");
+const fileUpload = document.querySelector('#file-upload');
+const image = document.querySelector('#uploaded-photo');
+const step1 =  document.querySelector('.add-post-modal__step-1');
+const step2 =  document.querySelector('.add-post-modal__step-2');
+const modalFooter = document.querySelector('.modal__footer');
+const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc1OTUzMDQ0LCJpYXQiOjE2NzExMTQ2NDQsImp0aSI6IjVjYThjMzJjNjUwYTRmZDI4OWQyMjFkMGNjZDA5NDEyIiwidXNlcl9pZCI6Mjh9.OTx5qCTfJZP-Y-gTO222zbMA_BejrdIjPP9XH48nozM";
+const locator = `https://c-gallery.polinashneider.space/api/v1/posts/`;
 
 function openModal(){
    modal.classList.add('active');        
@@ -36,23 +44,12 @@ document.addEventListener('keydown', function(e) {
 addPhotoBtn.addEventListener('click', openModal);
 addPostBtn.addEventListener('click', openModal);
 
-
-const fileUpload = document.querySelector('#file-upload');
-let image = document.querySelector('#uploaded-photo');
-const step1 =  document.querySelector('.add-post-modal__step-1');
-const step2 =  document.querySelector('.add-post-modal__step-2');
-const modalFooter = document.querySelector('.modal__footer');
-
 fileUpload.addEventListener('change', () => {
-    image.src = URL.createObjectURL(fileUpload.files[0]);     
-});
-
-fileUpload.addEventListener('click', () => {
+    image.src = URL.createObjectURL(fileUpload.files[0]);
     step1.classList.add('hidden');
     step2.classList.remove('hidden');
     modalFooter.classList.remove('hidden');
 });
-
 
 const postText = document.querySelector("#post-text");
 const postHashtags = document.querySelector("#post-hashtags");
@@ -63,25 +60,21 @@ function dataCleaning (){
   image.src = "./img/photo-post.jpg";
 };
 
-const publishBtn = document.querySelector("#post-publish");
-
-
 publishBtn.addEventListener('click', () => {
     const formData = new FormData();
     formData.append("image", fileUpload.files[0]);
     formData.append("text", postText.value);
     formData.append("tags", postHashtags.value);
 
-   const reply = fetch(`https://c-gallery.polinashneider.space/api/v1/posts/`, {
+   const reply = fetch(locator, {
         method: 'POST',
         body: formData,                 
         headers: {
-            'Authorization': 
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc1OTUzMDQ0LCJpYXQiOjE2NzExMTQ2NDQsImp0aSI6IjVjYThjMzJjNjUwYTRmZDI4OWQyMjFkMGNjZDA5NDEyIiwidXNlcl9pZCI6Mjh9.OTx5qCTfJZP-Y-gTO222zbMA_BejrdIjPP9XH48nozM",
+            'Authorization': token,
         },
     })
-    .then((rep) => {
-        if (rep.ok) {
+    .then((res) => {
+        if (res.ok) {
             showMessage();
         }
     })
@@ -113,3 +106,4 @@ function errorMessage () {
 };
 
 export {modal, addPhotoBtn, addPostBtn, body, bodyOverlay, openModal, closeModal, dataCleaning, showMessage, errorMessage};
+export {image, token, locator};
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
-'use strict';
 import {modal, addPhotoBtn, addPostBtn, body, bodyOverlay, openModal, closeModal, dataCleaning, showMessage, errorMessage } from "./create-post.js";
+import { addingPost } from "./display-post.js";
